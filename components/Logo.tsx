130149e07e7f88922952f3ea2fe7fd0af5d8e6ad
firatcnd <101863439+firatcnd@users.noreply.github.com>
2024-01-26 01:53:26 +0300

    Navbar changed

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Logo({ ...rest }) {
   return (
-    <Image src='/fcd.gif' id={"gif"} alt='' width={80} height={"50%"}/>
+    <Image src='/fcd_logo_new.png' id={"gif"} alt='' width={80} height={"40%"}/>
   );
 }
 
