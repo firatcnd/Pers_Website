0360bdfc9a19fa28fbe3eed9b262e1d89e955177
firatcnd <101863439+firatcnd@users.noreply.github.com>
2024-07-05 10:24:01 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -54,7 +54,7 @@ export default function Homepage() {
           <Features />
           {/*<Testimonials />*/}
           {/*<ScrollableBlogPosts posts={posts} />*/}
-          <Works />
+          {/*<Works />*/}
         </DarkerBackgroundContainer>
       </HomepageWrapper>
     </>
